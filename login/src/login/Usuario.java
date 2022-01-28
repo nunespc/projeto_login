0919d10952b137fc32b412c43d48de4f1d381176
Wanderson Nunes <87546860+nunespc@users.noreply.github.com>
2022-01-28 00:25:23 -0300

    Add files via upload

---
+++ login/src/login/Usuario.java
@@ -0,0 +1,28 @@
+package login;
+
+
+import java.util.Scanner;
+
+public class Usuario {
+	public static void main(String[] args) {
+		String usuario;
+		String senha;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Usuario: ");
+		usuario = entrada.next();
+		System.out.println("Senha: ");
+		senha = entrada.next();
+		
+		if(usuario.contains("Wanderson") && senha.contains("20102010")){
+			System.out.println("Usuario lougado com sucesso");
+		}else{
+			System.out.println("Por favor Verificar usuario ou senha invalido!");
+		}
+	
+	}
+	
+	
+
+}
